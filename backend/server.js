83327914a114fb73cee693b32a701bf47f4b0e94
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-12-08 22:06:51 +0530

    Enable JSON and urlencoded body parsing, and add signup endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => res.send('Server OK'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/signup', (req, res) => {
+    const { name, email, password } = req.body;
+    res.send({
+        message: "Signup endpoint working",
+        data: {name, email}
+    })
+})
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
