75608149bd9bffe22265a587df70580338fd2ee2
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-12-15 00:45:19 +0530

    feat(server): add material route mounting

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const workshopRoutes = require('./routes/workshopRoutes');
+const materialRoutes = require('./routes/materialRoutes');
 app.get('/', (req, res) => res.send({ message: "Workshop Management System" }));
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/workshops', workshopRoutes);
+app.use('/api/materials', materialRoutes);
 
 app.use((err, req, res, next) => {
     console.error(err);
