045116e21be60fb60389e593f66121cc39a62e84
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-12-17 00:57:12 +0530

    feat(server): enable CORS for frontend access

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
 dotenv.config();
@@ -10,6 +11,7 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
